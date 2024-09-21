30d389881371bcc8554b734a3216b5c0bc7dfe28
Oleksandr Buchek <oleksandrbuchek@gmail.com>
2024-09-21 16:31:59 +0200

    feat(rx-request): Add the provideRequestOptions util

--- libs/shared/util-effects/src/lib/providers/request-options.provider.ts
+++ libs/shared/util-effects/src/lib/providers/request-options.provider.ts
@@ -4,3 +4,7 @@ import { RxRequestOptions } from '../models';
 export const RequestOptions = createInjectionToken(
   (options: Pick<RxRequestOptions<any, any>, 'errorHandler' | 'store' | 'onError' | 'onSuccess'>) => options
 );
+
+export const provideRequestOptions = <Input = void, Response = unknown>(
+  options: Pick<RxRequestOptions<Input, Response>, 'errorHandler' | 'store' | 'onError' | 'onSuccess'>
+) => RequestOptions.provide(options);
